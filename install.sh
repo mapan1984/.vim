57aa886b94079d3bdece126810e159574900374e
mapan1984 <mapan1984@outlook.com>
2017-01-07 14:18:14 +0800

    add install.sh for initial configuration

---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/env bash
+
+printf "\033[33mChecking Start...\033[0m\n"
+
+
+printf "\033[36mChecking Git install...\033[0m\n"
+hash git 2>/dev/null || { echo >&2 "Require Git is not installed! Please install Git before you prefix config vim"; exit 1; }
+git --version
+printf "\033[36mChecking Git Completed!\033[0m\n"
+
+printf "\033[34mChecking Dir ...\033[0m\n"
+if [ ! -d "$HOME/.vim" ]; then
+	echo "Make ALL with $HOME Directory PATH"
+	printf "\n"
+	echo " [- .vim"
+	echo "   |- bundle   [ -- Manager Plugin -- ]"
+	echo "   |- autoload [ -- Manager Plugin -- ]"
+	echo "   |- .undo    [ -- Generator undo file -- ]"
+	echo "   |- .tmp     [ -- Generator tmp file -- ]"
+	echo " -] "
+	printf "\n"
+	
+	mkdir $HOME/.vim
+	mkdir $HOME/.vim/bundle
+	mkdir $HOME/.vim/.undo
+	mkdir $HOME/.vim/.tmp
+	mkdir $HOME/.vim/autoload
+else
+  if [ ! -d "$HOME/.vim/bundle" ]; then
+  	echo "Make dir -- bundle"
+  	mkdir $HOME/.vim/bundle
+  fi
+  
+  if [ ! -d "$HOME/.vim/autoload/" ]; then
+  	echo "Make dir -- [ autoload ]"
+  	mkdir $HOME/.vim/autoload/
+  fi
+  
+  if [ ! -d "$HOME/.vim/.undo" ]; then
+  	echo "Make dir -- [ .undo ]"
+  	mkdir $HOME/.vim/.undo
+  fi
+  
+  if [ ! -d "$HOME/.vim/.tmp" ]; then
+  	echo "Make dir -- [ .tmp ]"
+  	mkdir $HOME/.vim/.tmp
+  fi
+fi
+
+printf "\033[36mChecking plug.vim Exist?\033[0m\n"
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+	echo "Download Vim-Plug Plugin..."
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+printf "\033[36mChecking plug.vim Completed!\033[0m\n"
+
+printf "\033[34mChecking Dir Completed!\033[0m\n"
+
+printf "\033[32mCopy new files to the HOME PATH...\033[0m\n"
+
+printf "\n"
+echo " [- $HOME/"
+echo "   |- .vimrc       [ -- Vim config -- ]"
+echo " -] "
+printf "\n"
+
+# Copy file in to path
+#cp -f .vimrc $HOME/
+ln -f ./.vimrc ~/.vimrc
+
+printf "\033[32mCopy Completed, Finish Install !\033[0m\n"
+
+
+printf "\033[33mChecking Completed!\033[0m\n"
+printf "\n"
+exit 0;
